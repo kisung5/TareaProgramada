c77acb0a088dd4493f7bad4224380beb68a8d07f
Unknown <kisungqueso@hotmail.com>
2019-03-17 10:23:49 -0600

    Agregado de icono y FXML ids.

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,10 +12,12 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			primaryStage.getIcons().add(new Image("/icon.png"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,700,506);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Conversor-Hamming Binario");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/application/MainController.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class MainController {
+	
+	@FXML
+	private TextField inputText;
+	
+	@FXML 
+	private Button inputButton;
+	
+	@FXML
+	private Label message;
+	
+	@FXML
+	private ToggleGroup paridad;
+}
