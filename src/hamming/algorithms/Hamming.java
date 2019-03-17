4f4b5521167e557b756aac32353d2cea7498618a
Jose Ortega <ortega.josant@gmail.com>
2019-03-16 20:43:32 -0600

    Agregado el método para obtener el error de la hilera binaria

--- src/hamming/algorithms/Hamming.java
+++ src/hamming/algorithms/Hamming.java
@@ -4,34 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Hamming {
-	
-	public String getParity (String parityNum, int parity) {
-		int onumber = 0;
-		
-		for (char a : parityNum.toCharArray()) {
-			if ( a == '1') {
-				onumber++;
-			}
-		}
-//		Paridad impar
-		if ( parity == 0 ) {
-			if ( onumber%2 == 0) {
-				return "0";
-			} else {
-				return "1";
-			}
-		} 
-//		Paridad par
-		else {
-			if ( onumber%2 == 0) {
-				return "1";
-			} else {
-				return "0";
-			}
-		}
-	}
-	
-	public List<String> hammingCode(String binary) {
+	private boolean evenOrOdd;
+
+	/**
+	 * Método principal, se utiliza para realizar la paridad de una cadena de
+	 * binarios nueva
+	 * 
+	 * @param binary
+	 *            cadena con números binarios
+	 * @param parity
+	 *            True o False, true indica paridad Par, false indica paridad impar
+	 * @return Lista con Strings, cada indice es un 1 o 0 de la paridad realizada
+	 */
+	public List<String> hammingCode(String binary, boolean parity) {
+		this.evenOrOdd = parity;
 		int n = 0;
 
 		if (binary.length() == 1) {
@@ -43,17 +29,52 @@ public class Hamming {
 		} else {
 			n = 4;
 		}
-		System.out.println("El número es:" + n);
-		return hammingCodeAux(binary, n);
+		String hamming = hammingCodeAux(binary, n, false);
+		return parityList(hamming, hamming.length());
+	}
+	
+
+	/**
+	 * Se verifica el codigo binario
+	 * 
+	 * @param correctBinaryCode
+	 *            cadena con los binarios correctos
+	 * @param binaryCode
+	 *            cadena de Strings con todos los respectivos 1 y 0
+	 * @return Lista con String de 1 y 0, indica, la ultima posición de la lista
+	 *         tiene el índice en donde se encuentra el error, si este último es 0,
+	 *         significa que no hay error
+	 */
+	public List<String> errorDetection(String correctBinaryCode, String binaryCode) {
+		int n = (int) Math.sqrt(correctBinaryCode.length());
+		int cont = 0;
+		int index = 0;
+		String toDetect = hammingCodeAux(binaryCode, n, true);
+		int error = 0;
+		while (cont <= n) {
+			index = (int) Math.pow(2, cont) - 1;
+			if (correctBinaryCode.charAt(index) != (toDetect.charAt(index))) {
+				error += index + 1;
+			}
+			cont++;
+		}
+		return parityList(toDetect + error, toDetect.length());
 	}
 
-	private List<String> hammingCodeAux(String binary, int n) {
+	private String hammingCodeAux(String binary, int n, boolean detection) {
 		int cont = 0;
 		int parity = 0;
-		
-		String binaryExtended = makeSpaces(binary, n);
+		String binaryExtended;
+
+		if (detection) {
+			binaryExtended = binary;
+		} else {
+			binaryExtended = makeSpaces(binary, n);
+		}
 		String subString = "";
+		boolean first = false;
 		while (cont <= n) {
+			first = true;
 			for (int i = (int) (Math.pow(2, cont) - 1); i < binaryExtended.length(); i += (int) 2 * Math.pow(2, cont)) {
 
 				if (i + (int) Math.pow(2, cont) > binaryExtended.length()) {
@@ -61,36 +82,53 @@ public class Hamming {
 				} else {
 					subString = binaryExtended.substring(i, i + (int) Math.pow(2, cont));
 				}
-				System.out.println(subString);
 				while (subString.length() != 0) {
-					if (subString.charAt(0) == '1') {
-						parity++;
+					if (first) {
+						first = false;
+					} else {
+
+						if (subString.charAt(0) == '1') {
+							parity++;
+						}
 					}
 					subString = subString.substring(1, subString.length());
 				}
 			}
-			int pos = (int) Math.pow(2, cont)-1;
-			if ((int)parity%2 != 0) {
-				binaryExtended = new StringBuilder(binaryExtended).replace(pos, pos+1, "1").toString();	
+			int pos = (int) Math.pow(2, cont) - 1;
+			if ((int) parity % 2 != 0) {
+				if (evenOrOdd) {
+					binaryExtended = new StringBuilder(binaryExtended).replace(pos, pos + 1, "1").toString();
+				} else {
+					binaryExtended = new StringBuilder(binaryExtended).replace(pos, pos + 1, "0").toString();
+				}
+			} else {
+				if (evenOrOdd) {
+					binaryExtended = new StringBuilder(binaryExtended).replace(pos, pos + 1, "0").toString();
+				} else {
+					binaryExtended = new StringBuilder(binaryExtended).replace(pos, pos + 1, "1").toString();
+				}
 			}
-			
+
 			cont++;
-			System.out.println("pariedad: " + parity + "\n");
 			parity = 0;
 		}
 		
-		return parityList(binaryExtended);
+		return binaryExtended;
 	}
-	
-	private List<String> parityList(String binary) {
+
+	private List<String> parityList(String binary, int lenght) {
 		List<String> parityL = new ArrayList<String>();
-		for(int i = 0; i < binary.length(); i++ ) {
-			parityL.add(""+binary.charAt(i));
+		for (int i = 0; i < binary.length(); i++) {
+			if (i < lenght) { 
+			parityL.add("" + binary.charAt(i));
+			} else {
+				parityL.add(binary.substring(lenght));
+				break;
+			}
 		}
 		return parityL;
-		
+
 	}
-	
 
 	private String makeSpaces(String binary, int n) {
 		String str = "";
@@ -119,9 +157,7 @@ public class Hamming {
 			}
 			cont++;
 		}
-		System.out.println(str);
-		
-		System.out.println();
+
 		return str;
 	}
 }
